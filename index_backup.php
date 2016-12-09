31952c5cdd48651447f7a0578af1869560c46950
PLorei <loreipeter@gmail.com>
2016-12-08 20:02:15 -0500

    Delete index_backup.php

--- index_backup.php
+++
@@ -1,8 +0,0 @@
-<?php
-  include 'includes/functions.php';
-	include 'includes/header.php';
-	include 'includes/about.php';
-	include 'includes/home.php';
-	include 'includes/contact.php';
-	include 'includes/footer.php';
-?>
